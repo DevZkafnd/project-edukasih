98fe6f6ab47d2a865b70c1684156c321c84e0182
ZakiAffandi <toedoeng@gmail.com>
2025-12-22 17:31:51 +0700

    update

--- server/index.js
+++ server/index.js
@@ -10,8 +10,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+const allowedOrigin = process.env.CLIENT_ORIGIN || '*';
+const corsOptions = {
+  origin: allowedOrigin === '*' ? true : allowedOrigin,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 
 // Connect Database
